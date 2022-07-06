a43bb541d5f6a0780bf0fac67ca7eee36c851ee1
Lorel <lorel.oboue@gmail.com>
2022-07-06 01:35:46 +0100

    list of breweries showing on search but code to be simplified

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ const state = {
   breweries: [],
 };
 
-// Setup listener (currently just one) ...
-
 stateForm.addEventListener("submit", (event) => {
   console.log("my event", event, event.target);
   event.preventDefault();
@@ -39,9 +37,54 @@ stateForm.addEventListener("submit", (event) => {
 const renderBrewery = (brewery) => {
   const liEl = document.createElement("li");
 
-  liEl.innerHTML = `<h2>${brewery.name}</h2><h3> TODO: add the rest ...</h3>`;
+  const liHeader = document.createElement("h2");
+  liHeader.innerHTML = `${brewery.name}`;
+
+  const liDiv = document.createElement("div");
+  liDiv.setAttribute("class", "type");
+  liDiv.innerHTML = `${brewery.brewery_type}`;
+
+  const liSection = document.createElement("section");
+  liSection.setAttribute("class", "address");
+
+  const liH3 = document.createElement("h3");
+  liH3.innerText = "Address";
+  const liP = document.createElement("p");
+  liP.innerHTML = `${brewery.street}`;
+
+  const liCityAndPostcodePStrong = document.createElement("strong");
+  liCityAndPostcodePStrong.innerHTML = `${brewery.city}, ${brewery.postal_code} `;
+
+  const liSectionTwo = document.createElement("section");
+  liSectionTwo.setAttribute("class", "phone");
+  const liSectionTwoH3 = document.createElement("h3");
+  liSectionTwoH3.innerText = "Phone";
+  const liSectionTwoP = document.createElement("p");
+  liSectionTwoP.innerHTML = `${brewery.phone}`;
+
+  const liSectionThree = document.createElement("section");
+  liSectionThree.setAttribute("class", "link");
+  const liSectionThreeAttribute = document.createElement("a");
+  liSectionThreeAttribute.setAttribute("href", `${brewery.website_url}`);
+  liSectionThreeAttribute.setAttribute("target", "_blank");
+  liSectionThreeAttribute.innerText = "Visit Website";
+
+  //How do I set attributes with template literals
+  //   liEl.innerHTML = `<h2>${brewery.name}</h2>`;
+  // `<h3>${brewery.street}</h3>`
 
   breweriesListEl.append(liEl);
+  liEl.append(liHeader);
+  liEl.append(liDiv);
+  liEl.append(liSection);
+  liSection.append(liH3);
+  liSection.append(liP);
+  liSection.append(liCityAndPostcodePStrong);
+  liEl.append(liSectionTwo);
+  liSectionTwo.append(liSectionTwoH3);
+  liSectionTwo.append(liSectionTwoP);
+  liEl.append(liSectionThree);
+  liSectionThree.append(liSectionThreeAttribute);
 };
 
 const clearBreweries = () => {
